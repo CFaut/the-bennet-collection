53622d07505ac22758a52b0925847a513cdb71e3
CFaut <86919186+CFaut@users.noreply.github.com>
2024-01-09 10:08:21 +0200

    Basic routing for nav

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,17 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <div>
+       <div className='banner'>
         <h1>The Bennet Collection</h1>
+       </div>
+       <div className='nav'>
+         <Link href='/about'>About Us</Link>
+         <Link href='/shop'>Shop</Link>
+         <Link href='/custom'>Custom Made</Link>
+         <Link href='/contact'>Contact Us</Link>
        </div>
     </main>
   )
